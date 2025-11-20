60ad7bcf0e1c3725f4a21560fe98c24aeb21aac9
siva <ssankar6281@gmail.com>
2025-11-20 21:46:45 +0530

    second commit

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 // const heading=React.createElement("h1",{id:"heading"},"Hello world from React")
-const parent=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{id:"heading"},"I am h1 tag inside child1 div"),
-        React.createElement("h2",{id:"heading"},"I am h2 tag inside child1 div")
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{id:"heading"},"I am h1 tag inside child1 div"),
-        React.createElement("h2",{id:"heading"},"I am h2 tag inside child1 div")
-    ])
-])
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent);
+const heading=React.createElement("h1",{id:"heading"},"Hello world from React");
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
